6d5a766e31cca2a10ce53844be91ba46b22cc939
浪迹天涯 <1662531390@qq.com>
2021-02-18 11:54:18 +0800

    02.18

---
+++ 02.18/.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#define N 1000
+int a[N];
+int main(void)
+{
+	int n, i, count;
+	scanf("%d", &a[i]);
+	count = 0;
+	for (i = 1; i < n - 1; i++)
+		if ((a[i - 1]<a[i] && a[i]>a[i + 1]) || (a[i - 1] > a[i] && a[i] < a[i + 1]))
+			count++;
+	printf("%d\n", count);
+	return 0;
+}
